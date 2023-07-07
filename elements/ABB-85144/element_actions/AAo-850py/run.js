25f25851f5194f94ece7f80fc28e8de5b4bd977f
Claudio Eden <claudio.eden@gmail.com>
2023-07-07 16:25:30 -0300

    Automatic commit from Bubble

---
+++ elements/ABB-85144/element_actions/AAo-850py/run.js
@@ -0,0 +1,31 @@
+function(instance, properties, context) {
+
+	//context.uploadContent: function(fileName: String, contents: Base64 String, callback: function(err, url), [attachTo])
+    var uploadComplete = function(error, link){
+        if(link) {
+            console.log("UPLOADED", link);
+            instance.publishState("uploaded_file_url", link);
+            instance.publishState("error", false);
+            instance.publishState("error_body", "OK");
+            instance.triggerEvent("url_generated");
+        }
+        if(error) {
+            console.log(error);
+            instance.publishState("error", true);
+            instance.publishState("error_body", "Failure while Uploading Base64 File");
+        }
+    }
+    
+  	if(properties.file_name && properties.base64_string){
+        context.uploadContent(properties.file_name, properties.base64_string, function(err, url){uploadComplete(err, url)});
+    }
+    
+    //Load any data 
+
+
+
+  //Do the operation
+
+
+
+}
